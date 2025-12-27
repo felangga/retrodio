278f14516f3a1c6943267bf4a8fff3f1cea1f4a7
felangga <macbookair@192.168.1.13>
2025-12-27 15:01:42 +0700

    fix: set to WIB

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /*
  * config.h - Configuration file for Retrodio
- * 
+ *
  * Copyright (c) 2025 Felangga
- * 
+ *
  * This file contains all configuration settings for the radio application.
  * Modify these values according to your setup.
  */
@@ -14,16 +14,12 @@
 #define WIFI_SSID "KENARI"
 #define WIFI_PASSWORD "tamankenari"
 
-// ===== Radio Stream Configuration =====
-// swaragama  "http://202.65.114.229:9314/" 
-#define RADIO_URL "https://ig.idstreamer.com:8090/live"
-
 // ===== Audio Settings =====
 #define DEFAULT_VOLUME 5  // Range: 0-21
 
 // ===== NTP Configuration =====
 #define NTP_SERVER "pool.ntp.org"
-#define GMT_OFFSET_SEC 0      // Adjust for your timezone (e.g., 25200 for GMT+7)
+#define GMT_OFFSET_SEC 25200  // Adjust for your timezone (e.g., 25200 for GMT+7)
 #define DST_OFFSET_SEC 0      // Daylight saving time offset
 
-#endif // CONFIG_H
+#endif
